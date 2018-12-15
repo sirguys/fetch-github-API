3ef2139f7be77f208202223013f1a245875ebd99
sirguys <sirguys.apple@gmail.com>
2018-12-16 01:32:13 +0700

    service api

--- src/services/api.js
+++ src/services/api.js
@@ -3,34 +3,32 @@ import axios from 'axios'
 const BASE_URL = 'https://api.github.com'
 
 const fetchUsername = username => {
-    const URL = BASE_URL + `/users/${username}`
+  const URL = BASE_URL + '/users/' + username
 
-    return axios
-        .get(URL)
-        .then(res => res.data)
-        .catch(err => Promise.reject(err))
+  return axios
+    .get(URL)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err))
 }
 
 const fetchUserRepositories = username => {
-    const URL = BASE_URL + `/users/${username}/repos?sort=updated`
-
-    return axios
-        .get(URL)
-        .then(res => res.data)
-        .catch(err => Promise.reject(err))
+  const URL = `${BASE_URL}/users/${username}/repos?sort=updated`
+  return axios
+    .get(URL)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err))
 }
 
-const searchUsername = keyword => {
-    const URL = BASE_URL + `/search/users?per_page=32&q=${keyword}`
-
-    return axios
-        .get(URL)
-        .then(res => res.data)
-        .catch(err => Promise.reject(err))
+const searchUsername = username => {
+  const URL = `${BASE_URL}/search/users?q=${username}&per_page=32`
+  return axios
+    .get(URL)
+    .then(res => res.data)
+    .catch(err => Promise.reject(err))
 }
 
 export default {
-    fetchUsername,
-    searchUsername,
-    fetchUserRepositories
+  fetchUsername,
+  fetchUserRepositories,
+  searchUsername
 }
