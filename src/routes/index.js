6664da3157adbfbe0b4a60065f5452fd07905c63
sirguys <sirguys.apple@gmail.com>
2018-12-16 01:35:58 +0700

    set routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import Home from '../views/home'
+import UserDetail from '../views/users/UserDetail'
 
 const Routing = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/users/:username" component={UserDetail} />
   </Switch>
 )
 
