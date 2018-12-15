073cc2d7c0377f50338aca9247255f8386a209a0
sirguys <sirguys.apple@gmail.com>
2018-12-15 19:20:14 +0700

    setup services

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,36 @@
+import axios from 'axios'
+
 const BASE_URL = 'https://api.github.com'
-const LIST_USER = `${BASE_URL}/users:username`
-const LIST_USER_REPO = ''
+
+const fetchUsername = username => {
+    const URL = BASE_URL + `/users/${username}`
+
+    return axios
+        .get(URL)
+        .then(res => res.data)
+        .catch(err => Promise.reject(err))
+}
+
+const fetchUserRepositories = username => {
+    const URL = BASE_URL + `/users/${username}/repos?sort=updated`
+
+    return axios
+        .get(URL)
+        .then(res => res.data)
+        .catch(err => Promise.reject(err))
+}
+
+const searchUsername = keyword => {
+    const URL = BASE_URL + `/search/users?per_page=32&q=${keyword}`
+
+    return axios
+        .get(URL)
+        .then(res => res.data)
+        .catch(err => Promise.reject(err))
+}
+
+export default {
+    fetchUsername,
+    searchUsername,
+    fetchUserRepositories
+}
