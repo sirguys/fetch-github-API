daf69418405e47c88393a45768d23df40f1ca9e6
sirguys <sirguys.apple@gmail.com>
2019-01-01 20:10:26 +0700

    refactor with functional component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
 import AppHeader from './components/AppHeader'
 import Routing from './routes'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <AppHeader />
-        <Routing />
-      </div>
-    )
-  }
-}
+const App = () => (
+  <div className="App">
+    <AppHeader />
+    <Routing />
+  </div>
+)
 
 export default App
